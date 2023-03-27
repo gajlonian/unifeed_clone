0d81861606c88ddc2aab04f92798bfbdf3b6b4d2
Franckie <voninjatovofranckie@gmail.com>
2023-03-27 11:37:14 +0000

    Amelioration v1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var nav = document.querySelector(".nav")
+var btnNav = nav.querySelector(".nav__menu__btn")
+var navbar = nav.querySelector(".nav__navbar")
+
+btnNav.addEventListener("click", function() {
+	navbar.classList.toggle("open")
+})
